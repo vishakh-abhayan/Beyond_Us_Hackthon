e7e6e0ba6b98a4a29523c21aa483bb23644bc82d
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-07-08 17:22:31 +0530

    page fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello</h1>
+      <Home />
     </>
   );
 }
